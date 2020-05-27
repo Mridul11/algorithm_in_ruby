d864129095e114475cb8d936c40b51648ad8b3e6
Mridul11 <mridulmishra2991@gmail.com>
2020-05-27 14:34:33 +0530

    adding the shift and unshift method in linkedlist

---
+++ linkedlist.rb
@@ -0,0 +1,68 @@
+class Node
+    attr_accessor :next_node , :value 
+    def initialize(value, next_node=nil)
+        @value = value
+        @next_node = next_node
+    end
+end
+
+class LinkedList
+    def initialize(value)
+        @head = Node.new value         
+    end
+
+    def add val 
+        current = @head 
+        while !current.next_node.nil?
+            current  = current.next_node
+        end
+        current.next_node = Node.new val
+    end
+
+    def shift val 
+        current = Node.new val  
+        while true 
+            if @head == nil 
+                @head = current
+                break  
+            else
+                tmp = @head 
+                @head = current
+                @head.next_node = tmp 
+                break
+            end
+        end
+    end
+
+    def unshift
+        current = @head 
+        if current != nil 
+            # tmp = current 
+            tmp_next = current.next_node 
+            @head = tmp_next
+        end
+    end
+
+    def show
+        current = @head 
+        while(current != nil)
+            p current.value
+            # p current
+            # p current.next_node.nil?
+            current = current.next_node 
+        end
+    end
+end
+
+ll = LinkedList.new 0
+
+5.times do |i| 
+    ll.add i 
+end
+
+# ll.show 
+ll.shift 121
+ll.show 
+p "========================"
+ll.unshift
+ll.show
--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 # slicing a List or string and hash 
 str = "I am Mridul Mishra!"
 lst = ['mridul', 'mishra', 'vipul', 'mishra']
-p str.slice(0...3)
+p str.slice(0...6)
 p str[0,6]
 p lst.slice(0...3)
 p lst[0][0,3]
@@ -9,3 +9,18 @@ p lst[0][0,3]
 hash = {Name: "Mridul Mishra", age: "27"}
 p hash.slice(:age)
 p hash.slice(:Name)
+p " demo slicing"
+
+# stack implementation in ruby .
+stack = []
+stack << 12
+stack << "Mridul"
+stack.pop 
+p stack 
+
+# queue implementation in ruby 
+q = Queue.new 
+q << 12 
+q << 13 
+p q.pop 
+p q
